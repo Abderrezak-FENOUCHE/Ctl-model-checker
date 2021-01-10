01d5523a6ba213e57e1c7e34e46384c22fff7bfc
Abderrezak-FENOUCHE <abfenouche@gmail.com>
2021-01-10 10:39:18 +0100

    projet initialisé, installation et config de webpack et toutes les dépendances du projet

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path=require('path')
+const mode=process.env.isDev ?'development':'production'
+console.log(mode);
+module.exports={
+    mode,
+    entry:{
+        index:'./src/index.js'
+    },
+    output:{
+        path: path.resolve(__dirname,'dist'),
+        filename:'js/[name].js'
+ 
+    },
+    module: {
+        rules: [
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          },
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          }
+        ]
+      }
+}
